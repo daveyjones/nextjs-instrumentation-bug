6e53b4a6ac7ac03d2ff14f55c55d1836e4495686
David Jones <david@prodatakey.com>
2023-12-28 08:01:42 -0800

    Added instrumentation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  experimental: { instrumentationHook: true },
+}
 module.exports = nextConfig
---
+++ src/instrumentation.ts
@@ -0,0 +1,3 @@
+export function register() {
+  console.log("The register function was called.")
+}
